8e8c84421cf866501d45e59a6deea73c62b016df
Kathy Lau <imobilep501@gmail.com>
2016-10-04 22:19:14 -0400

    k

--- app.py
+++ app.py
@@ -1,24 +1,52 @@
 from flask import Flask, render_template, request
+import hashlib
 
 app = Flask(__name__)
 
-@app.route("/")
+def add(name, pazz, file):
+    f = open(file, 'a+')
+    myHashObj = hashlib.sha1(pazz)
+    pazz = myHashObj.hexdigest()
+    lines = f.readlines()
+    for line in lines:
+        print "LINE " + line
+        for names in line.split(','):
+            if name == names[0]:
+                return False
+    f.write(name + ',' + pazz + "\n")
+    return True
+
+def verify(name, pazz, file):
+    f = open(file, 'r')
+    lines = f.readlines()
+    myHashObj = hashlib.sha1(pazz)
+    pazz = myHashObj.hexdigest()
+    for line in lines:
+        if name == line.split(',')[0]:
+            return line.split(',')[1].replace('\n','') == pazz
+
+
+@app.route("/", methods=["GET", "POST"])
 def root():
-       #only works for POST
+    if request.method=="POST":
+        user = request.form["username"]
+        pazz = request.form["pazz"]
+        if add(user, pazz, 'auth.txt'):
+            return render_template("success.html", user=user)
     return render_template("index.html")
 
-@app.route("/auth", methods=["GET", "POST"])
-def ausdg():
-        #print "***DIAG: this Flask obj ***"
-        #print app
-        #print "***DIAG: request obj ***"
-        #print request
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
         if request.method=="POST":
-            if request.form["username"]=="Kathy" and request.form['pazz']=="123":
-                return render_template("success.html", user=request.form["username"])
+            user = request.form["username"]
+            pazz = request.form["pazz"]
+            if verify(user, pazz, "auth.txt"):
+                return "works"
             else:
-                return render_template("index.html")
+                return "no"
+        else:
+            return render_template("login.html")
 
 
 if __name__ == '__main__':
