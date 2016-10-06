62145cb3a19eebd08852dabff15678287a15e36e
Kathy Lau <imobilep501@gmail.com>
2016-10-06 13:45:36 -0400

    done

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = 'dcb61f28eafb8771213f3e0612422b8d'
+
 
 def add(name, pazz, file):
     f = open(file, 'a+')
@@ -28,15 +30,21 @@ def verify(name, pazz, file):
 
 @app.route("/", methods=["GET", "POST"])
 def root():
+
+    if "username" in session:
+        return redirect("login")
+
     if request.method=="POST":
         user = request.form["username"]
         pazz = request.form["pazz"]
         if add(user, pazz, '/data/auth.txt'):
             return render_template("success.html", user=user)
-    return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
+@app.route("/login/", methods=["GET", "POST"])
 def login():
         if request.method=="POST":
             user = request.form["username"]
@@ -51,6 +59,13 @@ def login():
                 return "works"
             else:
                 return render_template("login.html")
+
+@app.route("/logout", methods=["GET", "POST"])
+@app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    if "username" in session:
+        session.pop("username")
+    return redirect('/')
 
 
 if __name__ == '__main__':
